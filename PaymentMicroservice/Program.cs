e8938390b9b5ad7af91e0d804803fce3883c7c9c
unknown <michaelzeakis13@gmail.com>
2024-08-11 15:07:43 +0300

    consumer producer tests

--- PaymentMicroservice/Program.cs
+++ PaymentMicroservice/Program.cs
@@ -26,12 +26,11 @@ finally
     await busControl.StopAsync();
 }
 
-class InvoiceCreatedConsumer : IConsumer<IInvoiceCreated>
+public class InvoiceCreatedConsumer : IConsumer<IInvoiceCreated>
 {
     public async Task Consume(ConsumeContext<IInvoiceCreated> context)
     {
         await Task.Run(() =>
-        Console.WriteLine($"Received message for invoice number: {context.
-        Message.InvoiceNumber}"));
+        Console.WriteLine($"Received message for invoice number: {context.Message.InvoiceNumber}"));
     }
 }
